fe6c441310a2c545ac5696b8358b3f19bdd39b4b
Alexandre Papanis <alexandre.f.ext@e-ai.com.br>
2023-01-24 17:30:31 -0300

    First Commit - Integrating ChatGPT

---
+++ Horoscope/ContentView.swift
@@ -0,0 +1,81 @@
+//
+//  ContentView.swift
+//  Horoscope
+//
+//  Created by Alexandre Papanis on 24/01/23.
+//
+
+import OpenAISwift
+import SwiftUI
+
+final class ViewModel: ObservableObject {
+    init() {}
+    
+    private var client: OpenAISwift?
+    
+    func setup() {
+        client = OpenAISwift(authToken: "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")
+    }
+    
+    func send(text: String,
+              completion: @escaping (String) -> Void) {
+        client?.sendCompletion(with: text,
+                               maxTokens: 500,
+                               completionHandler: { result in
+            switch result {
+            case .success(let model):
+                let output = model.choices.first?.text ?? ""
+                completion(output)
+            case .failure:
+                break
+            }
+        })
+    }
+}
+
+struct ContentView: View {
+    @ObservedObject var viewModel = ViewModel()
+    @State var text = ""
+    @State var models = [String]()
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            ForEach(models, id: \.self) { string in
+                Text(string)
+            }
+            
+            Spacer()
+            
+            HStack {
+                TextField("Type here...", text: $text)
+                Button("send") {
+                    send()
+                }
+            }
+        }
+        .onAppear {
+            viewModel.setup()
+        }
+        .padding()
+    }
+    
+    func send(){
+        guard !text.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        models.append("Me: \(text)")
+        viewModel.send(text: text) { response in
+            DispatchQueue.main.async {
+                self.models.append("Horoscope: " + response)
+                self.text = ""
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Horoscope/PapaChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoroscopeApp.swift
+//  Horoscope
+//
+//  Created by Alexandre Papanis on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HoroscopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
